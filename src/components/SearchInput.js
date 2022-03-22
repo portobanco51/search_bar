13d1cbc23283719cd9f301e81b2184a165a0304a
Marcelo <marcelo011299@gmail.com>
2022-03-21 22:13:45 -0600

    stateHandler

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
 export class SearchInput extends React.Component {
+
+    state = { entry: '' }
+
+    onFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.entry)
+    }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <div className='ui massive icon input'>
-                            <label for='input' className='label'>Search: </label>
-                            <input type='text' name='input' className='' placeholder='Type anything!'></input>
+                            <label htmlFor='input' className='label'>Search: </label>
+                            <input type='text' name='input' className='' placeholder='Type anything!' onChange={(e) => { this.setState({ entry: e.target.value }) }} value={this.state.entry}></input>
                             <i className='search icon'></i>
                         </div>
                     </div>
-                </form>
+                </form >
             </div >
         )
     }
